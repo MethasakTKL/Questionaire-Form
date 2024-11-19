da7351a8010ac74554db11494178311535a02b07
MethasakTKL <methasak.tkl@outlook.com>
2024-11-19 17:39:16 +0700

    [fixed] rename file components to capitalize case

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import NavBar from "./components/navbar";
-import QuestionForm from "./components/questionform";
+import NavBar from "./components/NavBar";
+import QuestionForm from "./components/QuestionForm";
 
 import "@fontsource/prompt";
 
