82c5d13d23b4a5747a95fae0cfe82ec08b9ffeef
MethasakTKL <methasak.tkl@outlook.com>
2024-11-20 17:46:24 +0700

    [fixed] meterial ui theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,20 @@ import { Box } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import NavBar from "./components/NavBar";
 import QuestionForm from "./components/QuestionForm";
+import {Prompt} from "next/font/google";
+
+
+const prompt = Prompt({
+  weight: ["100", "300", "400", "700", "900"],
+  style: ["normal"],
+  subsets: ["latin"],
+});
 
 const theme = createTheme({
   typography: {
-    fontFamily: "Prompt, sans-serif",
+    fontFamily: prompt.style.fontFamily,
   },
 });
-
 export default function Home() {
   return (
     <ThemeProvider theme={theme}>
