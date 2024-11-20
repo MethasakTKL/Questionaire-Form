1f3f84a10a1aba3b9a0a0a374e5567d039b282f7
MethasakTKL <methasak.tkl@outlook.com>
2024-11-20 16:17:53 +0700

    [fixed] Error components import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
 import { Box } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-
-import NavBar from "./components/navbar";
-import QuestionForm from "./components/questionform";
+import NavBar from "./components/NavBar";
+import QuestionForm from "./components/QuestionForm";
 
 const theme = createTheme({
   typography: {
@@ -14,7 +13,7 @@ export default function Home() {
   return (
     <ThemeProvider theme={theme}>
       <Box>
-        <NavBar />
+        <NavBar/>
         <QuestionForm />
       </Box>
     </ThemeProvider>
